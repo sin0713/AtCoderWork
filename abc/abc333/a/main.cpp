63340e1c4f8e562bc407800e26d161603f976cf0
sin0713 <narisin1124@gmail.com>
2023-12-27 08:04:21 +0900

    abc333 a

---
+++ abc/abc333/a/main.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <atcoder/all>
+#include <math.h>
+
+using namespace std;
+
+// マクロ
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define INF (1 << 30)
+
+int main() {
+  int n;
+  cin >> n;
+
+  rep(i, n) {
+    cout << n;
+  }
+  cout << endl;
+
+
+
+
+
+
+  return 0;
+}
