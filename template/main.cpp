a6cfc15c58df3c1de8dcab05e9c1d1aa2aa34fb1
sin0713 <narisin1124@gmail.com>
2024-02-14 08:24:31 +0900

    template更新

--- template/main.cpp
+++ template/main.cpp
@@ -3,12 +3,14 @@
 #include <iostream>
 #include <atcoder/all>
 #include <math.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 // マクロ
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
-#define INF (1 << 30)
+#define INF 1e18
+using ll = long long;
 
 int main() {
   return 0;
