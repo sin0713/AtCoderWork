7076be6545efd6ae265980427b6cdbe3467cbedf
sin0713 <narisin1124@gmail.com>
2024-02-18 09:56:51 +0900

    abc318 D 修正

---
+++ abc/abc318/d/main.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <atcoder/all>
+#include <math.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// マクロ
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define INF 1e18
+using ll = long long;
+
+int main() {
+  return 0;
+}
