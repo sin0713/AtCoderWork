18d5d2defa7fcc749a724fde379aab82cb79d8b1
sin0713 <narisin1124@gmail.com>
2024-02-18 11:31:43 +0900

    abc318 D

--- abc/abc318/d/main.cpp
+++ abc/abc318/d/main.cpp
@@ -13,5 +13,40 @@ using namespace std;
 using ll = long long;
 
 int main() {
+  int n;
+  cin >> n;
+
+  vector<vector<int>> d(n, vector<int>(n+1));
+  rep(i, n-1) {
+    for (int j = i+1; j < n; j++) {
+      cin >> d[i][j];
+      d[j][i] = d[i][j];
+    }
+  }
+
+  // 重み無し頂点追加
+  // あまりの選択を作るため
+  if (n % 2 == 1) { n++; } 
+
+  ll ans = 0;
+  vector<bool> used(n); 
+  auto f = [&] (auto f, ll w) -> void {
+    ans = max(ans, w);
+    int i = 0;
+    while(i < n && used[i]) i++;
+    if (i == n) return; // usedの中身が全てtrue
+    used[i] = true;
+    for (int j = i+1; j < n; j++) {
+      if (used[j]) continue;
+
+      used[j] = true;
+      f(f, w+d[i][j]);
+      used[j] = false;
+    }
+    used[i] = false;
+  };
+
+  f(f, 0);
+  cout << ans << endl;
   return 0;
 }
