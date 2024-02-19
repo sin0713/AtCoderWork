9bf40dd6f9fdb383dd9b8c8484ad211e9c001b3f
sin0713 <narisin1124@gmail.com>
2024-02-19 22:35:35 +0900

    abc317 D

---
+++ abc/abc317/d/main.cpp
@@ -0,0 +1,45 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <atcoder/all>
+#include <math.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// マクロ
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+using ll = long long;
+
+void chmin(ll& x, ll y) { x = min(x, y); }
+
+int main() {
+  int n;
+  cin >> n;
+  int w = 0;
+
+  vector<pair<int, int>> data;
+  rep(i, n) {
+    int x, y, z;
+    cin >> x >> y >> z;
+    int a = max(0, (y-x+1) / 2);
+    w += z;
+    data.emplace_back(a, z*2);
+  }
+
+  const ll INF = 1e18;
+  vector<vector<ll>> dp(n+1, vector<ll>(w+1, INF));
+  dp[0][0] = 0;
+
+  rep(i, n) {
+    auto [a, z] = data[i];
+    rep(j, w + 1) {
+      chmin(dp[i+1][j], dp[i][j]);
+      chmin(dp[i][min(w, j+w)], dp[i][j]+a);
+    }
+  }
+
+  cout << dp[n][w] << endl;
+
+  return 0;
+}
