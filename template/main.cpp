351a33d759d71a447be502ebf360b5270d53e380
sin0713 <narisin1124@gmail.com>
2024-03-06 21:55:56 +0900

    テンプレート更新

--- template/main.cpp
+++ template/main.cpp
@@ -1,16 +1,12 @@
-#include <vector>
-#include <algorithm>
-#include <iostream>
 #include <atcoder/all>
-#include <math.h>
 #include <bits/stdc++.h>
 
 using namespace std;
 
 // マクロ
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
-#define INF 1e18
 using ll = long long;
+using P = pair<int, int>;
 
 int main() {
   return 0;
