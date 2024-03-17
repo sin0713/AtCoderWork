5d2c703b410804ca26fbce64dba0144c0f931433
sin0713 <narisin1124@gmail.com>
2024-03-17 22:51:10 +0900

    abc345 D

--- abc/abc345/d/main.cpp
+++ abc/abc345/d/main.cpp
@@ -15,7 +15,11 @@ int main() {
   vector<P> ab(n);
   rep(i, n) cin >> ab[i].first >> ab[i].second;
 
-  auto dfs = [&] (auto dfs, vector<string> board,  vector<bool> used) {
+  auto dfs = [&] (
+    auto dfs, 
+    vector<string> board,  
+    vector<bool> used
+  ) -> bool {
     int si = -1; int sj = -1; 
     rep(i, h) rep(j, w) {
       if (board[i][j] == '.' && si == -1) {
@@ -24,11 +28,42 @@ int main() {
     }
 
     if (si == -1) {
-      return true;
       cout << "Yes" << endl;
+      return true;
     }
 
+    rep(ti, n) { // タイル
+      if (used[ti]) continue;
+
+      auto [a, b] = ab[ti];
+      rep(ri, 2) { // 回転
+        swap(a, b);
+        if (si + a > h || sj + b > w) continue;
+
+        vector<string> ns = board;
+        bool ok = true;
+        // タイル配置
+        rep(ai, a) {
+          rep(bi, b) {
+            if (ns[si+ai][sj+bi] != '.') ok = false;
+
+            ns[si+ai][sj+bi] = '1'+ti;
+          }
+        }
+        if (!ok) continue; 
+
+        used[ti] = true;
+        if (dfs(dfs, ns, used)) return true;
+        used[ti] = false;
+      }
+    }
+
+    return false;
   };
 
+  
+  if (!dfs(dfs, vector<string>(h, string(w, '.')), vector<bool>(n))) {
+    cout << "No" << endl;
+  }
   return 0;
 }
