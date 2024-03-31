4d05aac74c65817960d80ba0b23030f03dae20f1
sin0713 <narisin1124@gmail.com>
2024-03-31 17:17:04 +0900

    abc347 C

--- abc/abc347/c/main.cpp
+++ abc/abc347/c/main.cpp
@@ -12,23 +12,27 @@ int main() {
   ll n, a, b;
   cin >> n >> a >> b;
 
-  ll m = a+b;
+  ll w = a+b;
   vector<ll> d(n);
   rep(i, n) { 
     ll x;
     cin >> x;
 
-    x %= m;
+    x %= w;
     d[i] = x;
   }
 
   sort(d.begin(), d.end());
-  ll min = d[0];
-  ll mx = d[n-1];
-  ll range = mx - min;
-  range++;
+  rep(i, n) d.push_back(d[i]+w);
 
-  if (range <= a) cout << "Yes" << endl;
-  else cout << "No" << endl;
+  rep(i, n) {
+    int l = d[i]; int r = d[i+n-1];
+    if (r-l+1 <= a) {
+      cout << "Yes" << endl;
+      return 0;
+    }
+  }
+  cout << "No" << endl;
+
   return 0;
 }
