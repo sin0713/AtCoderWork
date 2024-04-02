b0ea0b63136699e81aa1e9c733cc18145a934690
sin0713 <narisin1124@gmail.com>
2024-04-02 23:17:10 +0900

    abc285 D

--- abc/abc285/d/main.cpp
+++ abc/abc285/d/main.cpp
@@ -20,17 +20,26 @@ int main() {
   rep(i, n) { to[s[i]] = t[i]; }
 
   map<string, int> seen;
+  bool ok = true;
   auto dfs = [&] (auto dfs, string v, int cnt) -> void {
-    if (seen.count(v)) return;
+    if (seen.count(v)) {
+      if (seen[v] == cnt) {
+        ok = false;
+        return;
+      }
+
+      return;
+    }
 
     seen.insert({v, cnt});
-    dfs(dfs, to[v], cnt);
+    if (to.count(v)) {
+      dfs(dfs, to[v], cnt);
+    }
   };
 
   for (int i = 0; i < n; i++) {
-    dfs(dfs, to[s[i]], i);
-
-    if (seen.count(s[i]) && seen[s[i]] == i) {
+    dfs(dfs, s[i], i);
+    if (!ok) {
       cout << "No" << endl;
       return 0;
     }
