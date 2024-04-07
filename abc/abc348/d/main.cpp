c3b0e6cb5e743694293fc9ef652272aa46baf4f1
sin0713 <narisin1124@gmail.com>
2024-04-07 09:36:16 +0900

    abc348 D

--- abc/abc348/d/main.cpp
+++ abc/abc348/d/main.cpp
@@ -11,6 +11,13 @@ using P = pair<int, int>;
 vector<int> dx = {0, 0, -1, 1};
 vector<int> dy = {-1, 1, 0, 0};
 
+struct Med {
+  int r, c, e;
+
+  Med(){};
+  Med(int r, int c, int e): r(r), c(c), e(e) {};
+};
+
 int main() {
   int h, w;
   cin >> h >> w;
@@ -32,60 +39,73 @@ int main() {
 
   int n;
   cin >> n;
-  vector<int> r(n);
-  vector<int> c(n);
-  vector<int> e(n);
+  vector<Med> meds(n);
   rep(i, n) {
-    int a, b, d;
-    cin >> a >> b >> d;
+    int a, b, e;
+    cin >> a >> b >> e;
     a--;
     b--;
-    r[i] = a;
-    c[i] = b;
-    e[i] = d;
+
+    meds[i] = Med(a, b, e);
   }
-  map<P, int> mp;
-  rep(i, n) { mp[pair(r[i], c[i])] = e[i]; }
+  meds.emplace_back(start.first, start.second, 0);
+  meds.emplace_back(goal.first, goal.second, 0);
+  n+=2;
+  int si = n-2;
+  int gi = n-1;
 
   auto out_grid = [&](int i, int j) -> bool {
     return i < 0 || j < 0 || i >= h || j >= w;
   };
 
-  int energy = 0;
-  bool isOk = false;
-  vector<vector<bool>> seen(h, vector<bool>(w));
-  auto dfs = [&](auto dfs, int i, int j) -> void {
-    if (out_grid(i, j))
-      return;
-    if (seen[i][j]) return;
-    if (a[i][j] == '#')
-      return;
-    if (a[i][j] == 'T') {
-      isOk = true;
-      return;
+  const ll INF = 1e9;
+  vector<vector<int>> to(n);
+  rep(i, n) {
+    vector<vector<int>> dist(h, vector<int>(w, INF));
+    auto [r, c, e] = meds[i];
+    dist[r][c] = 0;
+    queue<P> q;
+    q.emplace(r, c);
+
+    // BFS
+    while(!q.empty()) {
+      auto [i, j] = q.front(); q.pop();
+
+      rep(dr, 4) {
+        int ni = i+dy[dr];
+        int nj = j+dx[dr];
+
+        if (out_grid(ni, nj)) continue;
+        if (a[ni][nj] == '#') continue;
+        if (dist[ni][nj] != INF) continue;
+
+        dist[ni][nj] = dist[i][j]+1;
+        q.emplace(ni, nj);
+      }
     }
 
-    P pos = pair(i, j);
-    if (mp.count(pos) && mp[pos] > energy) {
-      energy = mp[pos];
-      mp.erase(pos);
+    // 始点の薬からたどり着ける薬の辺をはる
+    rep(ni, n) if (i != ni) {
+      auto [rr, cc, ee] = meds[ni];
+      if (dist[rr][cc] <= e) to[i].push_back(ni);
     }
+  }
 
-    if (energy <= 0) {
-      return;
+  vector<int> can_reach(n, INF);
+  can_reach[si] = 0;
+  queue<int> q2;
+  q2.push(si);
+  while(!q2.empty()) {
+    int v = q2.front(); q2.pop();
+
+    for (auto u : to[v]) {
+      if (can_reach[u] != INF) continue;
+      can_reach[u] = can_reach[v]+1;
+      q2.push(u);
     }
+  }
 
-    seen[i][j] = true;
-    rep(k, 4) { 
-      dfs(dfs, i + dy[k], j + dx[k]); 
-    }
-    seen[i][j] = false;
-  };
-
-  dfs(dfs, start.first, start.second);
-  if (isOk)
-    cout << "Yes" << endl;
-  else
-    cout << "No" << endl;
+  if (can_reach[gi] != INF) cout << "Yes" << endl;
+  else cout << "No" << endl;
   return 0;
 }
