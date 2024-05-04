35a2b6091fa689f11be48be3d83059800c8c15d9
sin0713 <narisin1124@gmail.com>
2024-05-05 08:35:26 +0900

    abc352 D

--- abc/abc352/d/main.cpp
+++ abc/abc352/d/main.cpp
@@ -9,5 +9,33 @@ using ll = long long;
 using P = pair<int, int>;
 
 int main() {
+  int n, k;
+  cin >> n >> k;
+
+  vector<int> p(n);
+  rep(i, n) {
+    cin >> p[i];
+    p[i]--;
+  }
+
+  vector<int> q(n);
+  rep(i, n) q[p[i]] = i;
+
+  int ans = n;
+  set<int> st;
+  rep(i, n) {
+    st.insert(q[i]);
+
+    if (st.size() > k) {
+      st.erase(q[i-k]);
+    }
+
+    if (st.size() == k) {
+      int now = *st.rbegin() - *st.begin();
+      ans = min(ans, now);
+    }
+  }
+
+  cout << ans << endl;
   return 0;
 }
